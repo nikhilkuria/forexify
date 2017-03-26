68d1392bd7977a2d2c9c11ed577e5a67d07702e1
nikhilkuria <nikhilkuria@gmail.com>
2017-03-26 13:42:30 +0200

    rolling out v1.0.0

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = require("./lib");
+
+//forexify.convert(10,'EUR','INR', function(error,data){
+//  console.log(data);
+//});
+
+//forexify.supportedCurrencies(function(error,data){
+//  console.log(data);
+//});
