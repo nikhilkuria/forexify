bce90f84375686cd3e3cda1cbe27dde4fb88e009
nikhilkuria <nikhilkuria@gmail.com>
2017-04-09 15:07:57 +0200

    Adding the example in index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 module.exports = require("./lib");
 
-//forexify.convert(10,'EUR','INR', function(error,data){
-//  console.log(data);
-//});
+/**
+forexify.convert(10,'EUR','INR', function(error,data){
+  console.log(data);
+});
 
-//forexify.supportedCurrencies(function(error,data){
-//  console.log(data);
-//});
+forexify.supportedCurrencies(function(error,data){
+  console.log(data);
+});
+
+forexify.historicalRates("INR", "2013-10-30", function(error,data){
+  console.log(data);
+})
+**/
